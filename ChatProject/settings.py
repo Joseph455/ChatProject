200856b8cab9dcec7c24dd8852643db759ee5923
Joseph Adebowale <adebowalejojo2001@gmail.com>
2021-07-29 00:43:34 +0100

    commit

--- ChatProject/settings.py
+++ ChatProject/settings.py
@@ -47,7 +47,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     "django_filters",
-    'storages',
+    # 'storages',
     'channels',
     'chatapp',
     'userapp',
