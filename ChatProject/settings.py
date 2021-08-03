44150791c0e5d91074339749cd3e813f5522b174
Joseph Adebowale <adebowalejojo2001@gmail.com>
2021-08-03 21:34:35 +0100

    added  channels.auth.AuthMiddlewareStack to middleware

--- ChatProject/settings.py
+++ ChatProject/settings.py
@@ -64,6 +64,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'channels.auth.AuthMiddlewareStack',
 ]
 
 ROOT_URLCONF = 'ChatProject.urls'
