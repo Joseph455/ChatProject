ac79eee7d1470c0599f478677115e04d87bf7cc3
Joseph Adebowale <adebowalejojo2001@gmail.com>
2021-08-03 20:36:34 +0100

    fixed settings config

--- ChatProject/asgi.py
+++ ChatProject/asgi.py
@@ -14,10 +14,10 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 import chatApi.routing
 
+django.setup()
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'websocket.settings')
 
-django.setup()
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
